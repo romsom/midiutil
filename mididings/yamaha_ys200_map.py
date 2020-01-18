5fc22daaa2feaf1caf68d09b24326eb6e67a61f5
Roman Sommer <roman.sommer@fau.de>
2020-01-18 22:29:03 +0100

    yamaha_ys200_map: implement TX81Z sysex parameters

--- mididings/yamaha_ys200_map.py
+++ mididings/yamaha_ys200_map.py
@@ -53,113 +53,118 @@ class YS200_Patch:
     # - manage active and inactive patches
     def __init__(self, idx=None):
         self.params = {}
-        for op in range(0,6):
-            op_ofs = 21 * (5-op) # operator offset
+        # Voice Control parameters (VCED, g=4, h=2)
+        
+        # operator parameters
+        for op in range(0,4):
+            op_ofs = 13 * (3-op) # operator offset
             # 0
-            for i in range(0,4):
-                self.params['op{}_eg_rate_{}'.format(op+1, i+1)] = YS200_SXParamChange(0, 0, op_ofs + i, 99)
-                self.params['op{}_eg_level_{}'.format(op+1, i+1)] = YS200_SXParamChange(0, 0, op_ofs + 4 + i, 99)
-            # 8
-            itms = [('op{}_keyboard_level_scaling_break_point', 99),
-                    ('op{}_keyboard_level_scaling_left_depth', 99),
-                    ('op{}_keyboard_level_scaling_right_depth', 99),
-                    ('op{}_keyboard_level_scaling_left_curve', 3),
-                    ('op{}_keyboard_level_scaling_right_curve', 3),
-                    ('op{}_keyboard_rate_scaling', 7),
-                    ('op{}_amplitude_modulation_sensitivity', 3),
-                    ('op{}_key_velocity_sensitivity', 7),
-                    ('op{}_operator_output_level', 99),
-                    ('op{}_oscillator_mode', 1),
-                    ('op{}_oscillator_frequency_coarse', 31),
-                    ('op{}_oscillator_frequency_fine', 99),
-                    ('op{}_oscillator_detune', 14)]
-            for i, itm in enumerate(itms):
-                self.params[itm[0].format(op+1)] = YS200_SXParamChange(0,0,op_ofs + 8 + i, itm[1])
-            # 21=next_op
-        # global parameters
+            self.params['op{}_attack_rate'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs, 31)
+            self.params['op{}_decay_1_rate'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 1, 31)
+            self.params['op{}_decay_2_rate'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 2, 31)
+            self.params['op{}_release_rate'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 3, 15)
+            self.params['op{}_decay_1_level'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 4, 15)
+            self.params['op{}_level_scaling'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 5, 99)
+            self.params['op{}_rate_scaling'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 6, 3)
+            self.params['op{}_eg_bias_sensitivity'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 7, 7)
+            self.params['op{}_amplitde_modulation_enable'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 8, 1)
+            self.params['op{}_key_velocity_sensitivity'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 9, 7)
+            self.params['op{}_operator_output_level'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 10, 99)
+            self.params['op{}_frequency'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 11, 63)
+            self.params['op{}_detune'.format(op+1)] = YS200_SXParamChange(4, 2, op_ofs + 12, 6)
         
-        self.params['pitch_eg_rate_1'] = YS200_SXParamChange(0, 0, 126, 99)
-        self.params['pitch_eg_rate_2'] = YS200_SXParamChange(0, 0, 127, 99)
-        self.params['pitch_eg_rate_3'] = YS200_SXParamChange(0, 1, 0, 99)
-        self.params['pitch_eg_rate_4'] = YS200_SXParamChange(0, 1, 1, 99)
-        self.params['pitch_eg_level_1'] = YS200_SXParamChange(0, 1, 2, 99) # normal at 50/0x32
-        self.params['pitch_eg_level_2'] = YS200_SXParamChange(0, 1, 3, 99)
-        self.params['pitch_eg_level_3'] = YS200_SXParamChange(0, 1, 4, 99)
-        self.params['pitch_eg_level_4'] = YS200_SXParamChange(0, 1, 5, 99)
-        self.params['algorithm_select'] = YS200_SXParamChange(0, 1, 6, 31)
-        self.params['feedback'] = YS200_SXParamChange(0, 1, 7, 7)
-        self.params['oscillator_key_sync'] = YS200_SXParamChange(0, 1, 8, 1)
+        self.params['algorithm'] = YS200_SXParamChange(4, 2, 52, 7)
+        self.params['feedback'] = YS200_SXParamChange(4, 2, 53, 7)
         
-        self.params['lfo_speed'] = YS200_SXParamChange(0, 1, 9, 99)
-        self.params['lfo_delay'] = YS200_SXParamChange(0, 1, 10, 99)
-        self.params['lfo_pitch_modulation_depth'] = YS200_SXParamChange(0, 1, 11, 99)
-        self.params['lfo_amplitude_modulation_depth'] = YS200_SXParamChange(0, 1, 12, 99)
-        self.params['lfo_key_sync'] = YS200_SXParamChange(0, 1, 13, 1)
+        self.params['lfo_speed'] = YS200_SXParamChange(4, 2, 54, 99)
+        self.params['lfo_delay'] = YS200_SXParamChange(4, 2, 55, 99)
+        self.params['lfo_pitch_modulation_depth'] = YS200_SXParamChange(4, 2, 56, 99)
+        self.params['lfo_amplitude_modulation_depth'] = YS200_SXParamChange(4, 2, 57, 99)
+        self.params['lfo_key_sync'] = YS200_SXParamChange(4, 2, 58, 1)
         # tri, saw down, saw up, square, sine, s/h
-        self.params['lfo_wave'] = YS200_SXParamChange(0, 1, 14, 5)
-        self.params['lfo_pitch_modulation_sensitivity'] = YS200_SXParamChange(0, 1, 15, 7)
-        self.params['transpose'] = YS200_SXParamChange(0, 1, 16, 48) # normal at 24/0x18
+        self.params['lfo_wave'] = YS200_SXParamChange(4, 2, 59, 3)
+        self.params['lfo_pitch_modulation_sensitivity'] = YS200_SXParamChange(4, 2, 60, 7)
+        self.params['lfo_amplitude_modulation_sensitivity'] = YS200_SXParamChange(4, 2, 61, 3)
+        self.params['transpose'] = YS200_SXParamChange(4, 2, 62, 48) # normal at 24/0x18
 
-        self.params['voice_name_1'] = YS200_SXParamChange(0, 1, 17) # ascii
-        self.params['voice_name_2'] = YS200_SXParamChange(0, 1, 18)
-        self.params['voice_name_3'] = YS200_SXParamChange(0, 1, 19)
-        self.params['voice_name_4'] = YS200_SXParamChange(0, 1, 20)
-        self.params['voice_name_5'] = YS200_SXParamChange(0, 1, 21)
-        self.params['voice_name_6'] = YS200_SXParamChange(0, 1, 22)
-        self.params['voice_name_7'] = YS200_SXParamChange(0, 1, 23)
-        self.params['voice_name_8'] = YS200_SXParamChange(0, 1, 24)
-        self.params['voice_name_9'] = YS200_SXParamChange(0, 1, 25)
-        self.params['voice_name_10'] = YS200_SXParamChange(0, 1, 26)
-        self.params['operator_on/off'] = YS200_SXParamChange(0, 1, 27, 63) # bitmap for ops, lsb: op6
-        self.params['operator_select'] = YS200_SXParamChange(0, 1, 27, 5)
-        # DX Function Parameter Change
-        # group 1
-        self.params['dx_source_select'] = YS200_SXParamChange(1, 0, 1, 16, 1)
-        self.params['dx_poly/mono'] = YS200_SXParamChange(1, 0, 2, 1)
-        self.params['dx_pitch_bend_range'] = YS200_SXParamChange(1, 0, 3, 12)
-        self.params['dx_pitch_bend_step'] = YS200_SXParamChange(1, 0, 4, 12)
-        self.params['dx_portamento_time'] = YS200_SXParamChange(1, 0, 5, 99)
-        self.params['dx_portamento/glissando'] = YS200_SXParamChange(1, 0, 6, 1)
-        self.params['dx_portamento_mode'] = YS200_SXParamChange(1, 0, 7, 1)
-        # 8 not defined
-        self.params['dx_modulation_wheel_sensitivity'] = YS200_SXParamChange(1, 0, 9, 15)
-        self.params['dx_modulation_wheel_assign'] = YS200_SXParamChange(1, 0, 10, 7)
-        self.params['dx_foot_controller_sensitivity'] = YS200_SXParamChange(1, 0, 11, 15)
-        self.params['dx_foot_controller_assign'] = YS200_SXParamChange(1, 0, 12, 7)
-        self.params['dx_after_touch_sensitivity'] = YS200_SXParamChange(1, 0, 13, 15)
-        self.params['dx_after_touch_assign'] = YS200_SXParamChange(1, 0, 14, 7)
-        self.params['dx_breath_controller_sensitivity'] = YS200_SXParamChange(1, 0, 15, 15)
-        self.params['dx_breath_controller_assign'] = YS200_SXParamChange(1, 0, 16, 7)
-        # lots undefined
-        self.params['dx_audio_output_level_attenuator'] = YS200_SXParamChange(1, 0, 26, 7)
-        self.params['dx_master_tuning'] = YS200_SXParamChange(1, 0, 64)
+        self.params['poly/mono'] = YS200_SXParamChange(4, 2, 63, 1)
+        self.params['pitch_bend_range'] = YS200_SXParamChange(4, 2, 64, 12)
+        self.params['portamento_mode'] = YS200_SXParamChange(4, 2, 65, 1)
+        self.params['portamento_time'] = YS200_SXParamChange(4, 2, 66, 99)
+        self.params['foot_controller_volume'] = YS200_SXParamChange(4, 2, 67, 99)
+        self.params['sustain'] = YS200_SXParamChange(4, 2, 68, 1) # TODO check if foot controller
+        #self.params['foot_controller_assign'] = YS200_SXParamChange(4, 2, 73, 7)
+        self.params['portamento'] = YS200_SXParamChange(4, 2, 69, 1)
+        self.params['chorus'] = YS200_SXParamChange(4, 2, 70, 1)
+        self.params['modulation_wheel_pitch'] = YS200_SXParamChange(4, 2, 71, 99)
+        self.params['modulation_wheel_amplitude'] = YS200_SXParamChange(4, 2, 72, 99)
+        self.params['breath_controller_pitch'] = YS200_SXParamChange(4, 2, 73, 99)
+        self.params['breath_controller_amplitude'] = YS200_SXParamChange(4, 2, 74, 99)
+        self.params['breath_controller_eg_bias'] = YS200_SXParamChange(4, 2, 75, 99)
+        self.params['breath_controller_pitch_bias'] = YS200_SXParamChange(4, 2, 76, 99)
 
-        # Function Parameter Change
-        self.params['poly/mono'] = YS200_SXParamChange(2, 0, 64, 1)
-        self.params['pitch_bend_range'] = YS200_SXParamChange(2, 0, 65, 12)
-        self.params['pitch_bend_step'] = YS200_SXParamChange(2, 0, 66, 12)
-        self.params['portamento_mode'] = YS200_SXParamChange(2, 0, 67, 1)
-        self.params['portamento/glissando'] = YS200_SXParamChange(2, 0, 68, 1)
-        self.params['portamento_time'] = YS200_SXParamChange(2, 0, 69, 99)
-        self.params['modulation_wheel_sensitivity'] = YS200_SXParamChange(2, 0, 70, 99)
-        self.params['modulation_wheel_assign'] = YS200_SXParamChange(2, 0, 71, 7)
-        self.params['foot_controller_sensitivity'] = YS200_SXParamChange(2, 0, 72, 99)
-        self.params['foot_controller_assign'] = YS200_SXParamChange(2, 0, 73, 7)
-        self.params['breath_controller_sensitivity'] = YS200_SXParamChange(2, 0, 74, 99)
-        self.params['breath_controller_assign'] = YS200_SXParamChange(2, 0, 75, 7)
-        self.params['after_touch_sensitivity'] = YS200_SXParamChange(2, 0, 76, 99)
-        self.params['after_touch_assign'] = YS200_SXParamChange(2, 0, 77, 7)
+        
+        self.params['voice_name_1'] = YS200_SXParamChange(4, 2, 77) # ascii
+        self.params['voice_name_2'] = YS200_SXParamChange(4, 2, 78)
+        self.params['voice_name_3'] = YS200_SXParamChange(4, 2, 79)
+        self.params['voice_name_4'] = YS200_SXParamChange(4, 2, 80)
+        self.params['voice_name_5'] = YS200_SXParamChange(4, 2, 81)
+        self.params['voice_name_6'] = YS200_SXParamChange(4, 2, 82)
+        self.params['voice_name_7'] = YS200_SXParamChange(4, 2, 83)
+        self.params['voice_name_8'] = YS200_SXParamChange(4, 2, 84)
+        self.params['voice_name_9'] = YS200_SXParamChange(4, 2, 85)
+        self.params['voice_name_10'] = YS200_SXParamChange(4, 2, 86)
+        # 87 - 92 unused in TX81z
+        self.params['operator_on/off'] = YS200_SXParamChange(4, 2, 93, 15) # bitmap for ops, lsb: op4
+        # tx81 / ys200 done until here
+        
+        # ACED (g=4, h=3)
+        for op in range(0,4):
+            op_ofs = 5 * (3-op) # operator offset
+            # 0
+            self.params['op_{}_fixed_frequency'.format(op+1)] = YS200_SXParamChange(4, 3, op_ofs, 1)
+            self.params['op_{}_fixed_frequency_range'.format(op+1)] = YS200_SXParamChange(4, 3, op_ofs + 1, 7)
+            self.params['op_{}_frequency_range_fine'.format(op+1)] = YS200_SXParamChange(4, 3, op_ofs + 2, 15)
+            self.params['op_{}_waveform'.format(op+1)] = YS200_SXParamChange(4, 3, op_ofs + 3, 7)
+            self.params['op_{}_eg_shift'.format(op+1)] = YS200_SXParamChange(4, 3, op_ofs + 4, 3)
+        self.params['reverb_rate'] = YS200_SXParamChange(4, 3, 20, 7)
+        self.params['foot_controller_pitch'] = YS200_SXParamChange(4, 3, 21, 99)
+        self.params['foot_controller_amplitude'] = YS200_SXParamChange(4, 3, 22, 99)
 
-        # tx function parameters
-        self.params['data_entry_recieve_switch'] = YS200_SXParamChange(4, 0, 0, 1)
-        self.params['control_change_recieve_switch'] = YS200_SXParamChange(4, 0, 1, 1)
-        self.params['data_entry_volume_switch'] = YS200_SXParamChange(4, 0, 2, 1)
-        self.params['compute_communication_switch'] = YS200_SXParamChange(4, 0, 3, 1)
-        self.params['combined/individual'] = YS200_SXParamChange(4, 0, 4)
-        self.params['note_limit_low'] = YS200_SXParamChange(4, 0, 5)
-        self.params['note_limit_high'] = YS200_SXParamChange(4, 0, 6)
-        self.params['memory_protection_off/on'] = YS200_SXParamChange(4, 0, 7) # 0 or 127
-        self.params['load_function_select_int/ext'] = YS200_SXParamChange(4, 0, 11) # 0 or 127
+
+        # performance parameters (g=4, h=3)
+
+        for inst in range(0,8):
+            inst_ofs = 12 * inst
+            self.params['inst{}_maximum_notes'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs, 8)
+            self.params['inst{}_voice_number_msb'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 1, 1)
+            self.params['inst{}_voice_number'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 2)
+            self.params['inst{}_reveive_channel'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 3, 16) # 16: omni
+            self.params['inst{}_low_note_limit'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 4)
+            self.params['inst{}_high_note_limit'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 5)
+            self.params['inst{}_detune'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 6, 14)
+            self.params['inst{}_note_shift'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 7, 48)
+            self.params['inst{}_volume'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 8, 99)
+            self.params['inst{}_output_assign'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 9, 3)
+            self.params['inst{}_lfo_select'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 10, 99) # 0: off, 1: inst1, 2: inst2, 3: vib
+            self.params['inst{}_micro_tune_enable'.format(inst+1)] = YS200_SXParamChange(4, 3, inst_ofs + 11, 1)
+
+        self.params['micro_tune_table'] = YS200_SXParamChange(4, 3, inst_ofs + 96, 12)
+        self.params['assign_mode'] = YS200_SXParamChange(4, 3, inst_ofs + 97, 1)
+        self.params['effect'] = YS200_SXParamChange(4, 3, inst_ofs + 98, 3)
+        self.params['micro_tune_key'] = YS200_SXParamChange(4, 3, inst_ofs + 99, 11)
+        self.params['performance_name_1'] = YS200_SXParamChange(4, 3, inst_ofs + 100) # ascii
+        self.params['performance_name_2'] = YS200_SXParamChange(4, 3, inst_ofs + 101)
+        self.params['performance_name_3'] = YS200_SXParamChange(4, 3, inst_ofs + 102)
+        self.params['performance_name_4'] = YS200_SXParamChange(4, 3, inst_ofs + 103)
+        self.params['performance_name_5'] = YS200_SXParamChange(4, 3, inst_ofs + 104)
+        self.params['performance_name_6'] = YS200_SXParamChange(4, 3, inst_ofs + 105)
+        self.params['performance_name_7'] = YS200_SXParamChange(4, 3, inst_ofs + 106)
+        self.params['performance_name_8'] = YS200_SXParamChange(4, 3, inst_ofs + 107)
+        self.params['performance_name_9'] = YS200_SXParamChange(4, 3, inst_ofs + 108)
+        self.params['performance_name_10'] = YS200_SXParamChange(4, 3, inst_ofs + 109)
+            
+        # TODO: remote switch parameters for TX81z, YS200 (if avail)
 
     def parameters(self):
         return self.params
