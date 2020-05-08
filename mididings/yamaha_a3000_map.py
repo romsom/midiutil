bbc320d4b01798b70fcb73b3ffedbccb68d86132
Roman Sommer <roman.sommer@fau.de>
2020-05-09 01:54:08 +0200

    yamaha_a3000: fix and simplify switch number assignment

--- mididings/yamaha_a3000_map.py
+++ mididings/yamaha_a3000_map.py
@@ -5,8 +5,6 @@ def gen_remote_switch_sysex(ev, format):
     return md.event.SysExEvent(ev.port, [0xf0, 0x43, 0x10, 0x58, 0x03, format & 0x7f, 0x00, 0x00, 0x00, 0x00, 0x00, ev.value, 0xf7])
 
 class A3000_RemoteSwitch:
-    FKEY1, FKEY2, FKEY3, FKEY4, FKEY5, FKEY6, COMMAND, ASSIGNABLE, AUDITION, PLAY, EDIT, REC, DISK, UTILITY, KNO, KNOB1_SW, KNOB2_SW, KNOB3_SW, KNOB4_SW, KNOB5_SW = range(19)
-    KNOB1_ENCODER, KNOB2_ENCODER, KNOB3_ENCODER, KNOB4_ENCODER, KNOB5_ENCODER, KNOB6_ENCODER = range(123, 128)
     def __init__(self, number):
         self.number = number
     def event(self):
@@ -15,30 +13,31 @@ class A3000_RemoteSwitch:
 class A3000_RemoteControl:
     def __init__(self):
         self.params                  = {}
-        self.params['fkey1']         = A3000_RemoteSwitch(A3000_RemoteSwitch.FKEY1)
-        self.params['fkey2']         = A3000_RemoteSwitch(A3000_RemoteSwitch.FKEY2)
-        self.params['fkey3']         = A3000_RemoteSwitch(A3000_RemoteSwitch.FKEY3)
-        self.params['fkey4']         = A3000_RemoteSwitch(A3000_RemoteSwitch.FKEY4)
-        self.params['fkey5']         = A3000_RemoteSwitch(A3000_RemoteSwitch.FKEY5)
-        self.params['fkey6']         = A3000_RemoteSwitch(A3000_RemoteSwitch.FKEY6)
-        self.params['command']       = A3000_RemoteSwitch(A3000_RemoteSwitch.COMMAND)
-        self.params['assignable']    = A3000_RemoteSwitch(A3000_RemoteSwitch.ASSIGNABLE)
-        self.params['audition']      = A3000_RemoteSwitch(A3000_RemoteSwitch.AUDITION)
-        self.params['play']          = A3000_RemoteSwitch(A3000_RemoteSwitch.PLAY)
-        self.params['rec']           = A3000_RemoteSwitch(A3000_RemoteSwitch.REC)
-        self.params['disk']          = A3000_RemoteSwitch(A3000_RemoteSwitch.DISK)
-        self.params['utility']       = A3000_RemoteSwitch(A3000_RemoteSwitch.UTILITY)
-        self.params['kno']           = A3000_RemoteSwitch(A3000_RemoteSwitch.KNO)
-        self.params['knob1_sw']      = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB1_SW)
-        self.params['knob2_sw']      = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB2_SW)
-        self.params['knob3_sw']      = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB3_SW)
-        self.params['knob4_sw']      = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB4_SW)
-        self.params['knob5_sw']      = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB5_SW)
-        self.params['knob1_encoder'] = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB1_ENCODER)
-        self.params['knob2_encoder'] = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB2_ENCODER)
-        self.params['knob3_encoder'] = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB3_ENCODER)
-        self.params['knob4_encoder'] = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB4_ENCODER)
-        self.params['knob5_encoder'] = A3000_RemoteSwitch(A3000_RemoteSwitch.KNOB5_ENCODER)
+        self.params['fkey1']         = A3000_RemoteSwitch(0)
+        self.params['fkey2']         = A3000_RemoteSwitch(1)
+        self.params['fkey3']         = A3000_RemoteSwitch(2)
+        self.params['fkey4']         = A3000_RemoteSwitch(3)
+        self.params['fkey5']         = A3000_RemoteSwitch(4)
+        self.params['fkey6']         = A3000_RemoteSwitch(5)
+        self.params['command']       = A3000_RemoteSwitch(6)
+        self.params['assignable']    = A3000_RemoteSwitch(7)
+        self.params['audition']      = A3000_RemoteSwitch(8)
+        self.params['play']          = A3000_RemoteSwitch(9)
+        self.params['edit']          = A3000_RemoteSwitch(10)
+        self.params['rec']           = A3000_RemoteSwitch(11)
+        self.params['disk']          = A3000_RemoteSwitch(12)
+        self.params['utility']       = A3000_RemoteSwitch(13)
+        self.params['kno']           = A3000_RemoteSwitch(14)
+        self.params['knob1_sw']      = A3000_RemoteSwitch(14)
+        self.params['knob2_sw']      = A3000_RemoteSwitch(15)
+        self.params['knob3_sw']      = A3000_RemoteSwitch(16)
+        self.params['knob4_sw']      = A3000_RemoteSwitch(17)
+        self.params['knob5_sw']      = A3000_RemoteSwitch(19)
+        self.params['knob1_encoder'] = A3000_RemoteSwitch(123)
+        self.params['knob2_encoder'] = A3000_RemoteSwitch(124)
+        self.params['knob3_encoder'] = A3000_RemoteSwitch(125)
+        self.params['knob4_encoder'] = A3000_RemoteSwitch(126)
+        self.params['knob5_encoder'] = A3000_RemoteSwitch(127)
 
     def parameters(self):
         return self.params
